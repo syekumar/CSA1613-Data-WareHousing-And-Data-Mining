1d213f2ec9ba628131c0d6c192c9ed58a235005c
syekumar <syekumaruppu@gmail.com>
2026-07-24 12:09:02 +0530

    Delete DWDM directory

--- DWDM/Exp-20-Tennis Players Boxplot.R
+++
@@ -1,7 +0,0 @@
-points <- c(25,30,28,35,32,40,45,38,31,90)
-
-boxplot(points,
-        col="yellow",
-        main="Player Scores")
-
-boxplot.stats(points)$out
--- DWDM/Exp-7-Mean, Median, Mode (Pencils).R
+++
@@ -1,8 +0,0 @@
-pencils <- c(9,25,23,12,11,6,7,8,9,10)
-
-mean(pencils)
-
-median(pencils)
-
-table(pencils)
-names(which.max(table(pencils)))
